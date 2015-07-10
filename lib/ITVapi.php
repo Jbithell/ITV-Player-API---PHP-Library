31a923f95218c505c0588259773f816ef048e758
James Bithell <james@bithell.com>
2015-07-10 12:45:46 +0100

    Developed get all listings function

--- lib/ITVapi.php
+++ lib/ITVapi.php
@@ -9,7 +9,25 @@ class ITVapi extends API {
 	function getListings ($category) {
 		
 	}
-	
+	function getAllListings() {
+		$videos = array();
+		foreach (["A","B","C","D","E","F","G","H","I","J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] as $letter) {
+			$url = "http://mercury.itv.com/api/json/dotcom/programme/searchatoz/" . strtolower($letter);
+			$jsondata = file_get_contents($url);
+			$obj = json_decode($jsondata, true);
+			foreach ($obj["Result"][0]["Details"] as $items) {
+			$videos[] = new Show($items["Programme"]["Programme"]["Id"],
+								$items["Programme"]["Programme"]["Title"],
+								$items["Programme"]["Programme"]["ShortSynopsis"],
+								$items["Programme"]["Programme"]["LongSynopsis"],
+								$items["Programme"]["ImageUri"],
+								"",
+								"ITV"
+								);
+			}
+		}
+		return $videos;
+	}
 	function getShow($name) {
 		
 	}
@@ -21,7 +39,6 @@ class ITVapi extends API {
 	function search($q) {
 		$url = "http://mercury.itv.com/api/json/dotcom/programme/searchatoz/" . rawurlencode($q);
 		$jsondata = file_get_contents($url);
-		echo $url;
 		$obj = json_decode($jsondata, true);
 		$videos = array();
 		foreach ($obj["Result"][0]["Details"] as $items) {
@@ -38,5 +55,6 @@ class ITVapi extends API {
 	}
 }
 $YOUapi = new ITVapi ();
-print_r($YOUapi -> search("Coronation Street"));
+//print_r($YOUapi -> search("Coronation Street"));
+print_r($YOUapi -> getAllListings());
 ?>
